418c52a3c1a44bf7f0bf94c6cbe0e7bd5abfaf46
RYLiang18 <richard.y.liang@gmail.com>
2021-07-07 19:03:57 -0700

    playing around with sqlalchemy

--- test2.py
+++ test2.py
@@ -1,11 +1,16 @@
-import mysql.connector
+# import mysql.connector
 import os
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd = os.environ.get("mysql_root_pwd"),
-    database="linuxhint"
-)
+from sqlalchemy import create_engine
+
+# db = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     passwd = os.environ.get("mysql_root_pwd"),
+#     database="linuxhint"
+# )
+db_pwd = os.environ.get("mysql_root_pwd")
+engine = create_engine(f'mysql+mysqlconnector://root:{db_pwd}@localhost/robocorg')
+
 
 print("no errors? noice")
