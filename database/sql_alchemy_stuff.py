e2c2505ef13fdf9a9b6016155337b6b16708b972
RYLiang18 <richard.y.liang@gmail.com>
2021-07-07 21:53:03 -0700

    setting up robocorg rdb with sqlalchemy

--- database/sql_alchemy_stuff.py
+++ database/sql_alchemy_stuff.py
@@ -1,14 +1,8 @@
 # import mysql.connector
 import os
-
 from sqlalchemy import create_engine
 
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     passwd = os.environ.get("mysql_root_pwd"),
-#     database="linuxhint"
-# )
+# connecting the database
 db_pwd = os.environ.get("mysql_root_pwd")
 engine = create_engine(f'mysql+mysqlconnector://root:{db_pwd}@localhost/robocorg')
 
