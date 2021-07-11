ca123907a67ebd5e06be9a7c40d93de5a8ea4108
RYLiang18 <richard.y.liang@gmail.com>
2021-07-11 01:06:54 -0700

    created models for robocorg database

--- database/models.py
+++ database/models.py
@@ -1,13 +1,32 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
 
 base = declarative_base()
 
-class StreamerModel(Base):
-    __tablename__ = "streamer"
+class StreamerModel(base):
+    __tablename__ = "STREAMER_TBL"
+    id = Column(
+        'id', Integer, primary_key=True
+    )
+    twitch_name = Column('twitch_name', String(100))
+    discord_id = Column('discord_id', String(100))
+
+class SubscriberModel(base):
+    __tablename__ = "SUBSCRIBER_TBL"
     id = Column(
         'id',
         Integer,
         primary_key=True
     )
-    
+    phone_numbers = Column('phone_numbers', String(100))
+    discord_id = Column('discord_id', String(100))
+
+class RelationshipModel(base):
+    __tablename__ = "RELATIONSHIP_TBL"
+    streamer_id = Column(
+        'streamer_id', ForeignKey('STREAMER_TBL.id'), primary_key=True
+    )
+    subscriber_id = Column(
+        'subscriber_id', ForeignKey('SUBSCRIBER_TBL.id'), primary_key=True
+    )
--- database/sql_alchemy_stuff.py
+++ database/sql_alchemy_stuff.py
@@ -1,10 +1,19 @@
 # import mysql.connector
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+
+from models import (
+    base, StreamerModel, SubscriberModel, RelationshipModel
+)
 
 # connecting the database
 db_pwd = os.environ.get("mysql_root_pwd")
 engine = create_engine(f'mysql+mysqlconnector://root:{db_pwd}@localhost/robocorg')
+
+# create tables based on the models, i guess
+base.metadata.create_all(bind=engine)
+
 
 
 print("no errors? noice")
