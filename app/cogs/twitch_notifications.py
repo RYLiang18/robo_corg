b55ff14ceeaef9dd8393a58157e5a3bb9e7c8385
RYLiang18 <richard.y.liang@gmail.com>
2022-03-08 19:11:44 -0800

    refactor: refactored all main.py commands(subscribe_to + add_streamer) and twitch notifier to cogs

--- app/cogs/twitch_notifications.py
+++ app/cogs/twitch_notifications.py
@@ -1,7 +1,12 @@
 from discord.ext import commands, tasks
 from aux.twitch_aux import Twitch_Aux
+from aux.twilio_aux import Twilio_Aux
+from aux.other import (
+    check_phone_number_in_dms,
+    extract_phone_number
+)
 
-from database.models import StreamerModel
+from database.models import StreamerModel, SubscriberModel
 from main import Session
 
 from sqlalchemy import select
@@ -24,8 +29,17 @@ class Twitch_Notifications(commands.Cog):
             curr_streamer: Twitch_Aux = Twitch_Aux(db_streamer.twitch_name)
 
             if curr_streamer.is_live != db_streamer.is_live:
-                # subscriber_dict = dict()
-                # for subscruber in db_streamer
+
+                # //// texting ////////////////////
+                subscriber_dict = dict()
+                for subscriber in db_streamer.subscribers:
+                    sub_phone = subscriber.phone_number
+                    sub_name = self.client.get_user(int(subscriber.discord_id)).name
+                    subscriber_dict[sub_phone] = sub_name
+
+                twilio_aux = Twilio_Aux(subscriber_dict)
+                twilio_aux.send_messages(curr_streamer)
+                # /////////////////////////////////
 
                 if curr_streamer.is_live:
                     await ctx.send(
@@ -50,6 +64,74 @@ class Twitch_Notifications(commands.Cog):
     async def stop_twitch_notifs(self, ctx):
         await ctx.send("twitch live notifications STOPPING...")
         self.twitch_listener.stop()
+
+    @commands.command()
+    async def subscribe_to(self, ctx, twitch_name):
+        session = Session()
+        twilio_aux = Twilio_Aux()
+        sender_id = str(ctx.author.id)
+        # query Streamer with 'twitch_name' from STREAMER_TBL
+        streamer : StreamerModel = session.query(StreamerModel).filter(
+            StreamerModel.twitch_name.ilike(f"%{twitch_name}%")
+        ).first()
+
+        # query Subscriber with 'sender_id' from SUBSCRIBER_TBL
+        subscriber: SubscriberModel = session.query(SubscriberModel).filter(
+            SubscriberModel.discord_id.ilike(f"%{sender_id}%")
+        ).first()
+
+        if streamer is not None:
+            if subscriber is not None:
+                streamer.subscribers.append(subscriber)
+                ctx.send("done")
+            else:
+                # new subscriber,
+                # dm for phone number
+                await ctx.author.send(
+                    f"To receive text notifications for {streamer.twitch_name}, please enter your phone number\n"
+                    f"robo-corg promises to keep this information confidential!"
+                )
+
+                resp = (
+                    await self.client.wait_for('message', check=check_phone_number_in_dms)
+                ).content
+                phone_number = extract_phone_number(resp)
+                
+                # creating new subscriber based on schema and adding relationship
+                # to STREAMBER_TBL
+                new_sub = SubscriberModel(
+                    phone_number = phone_number,
+                    discord_id = str(ctx.author.id)
+                )
+                streamer.subscribers.append(new_sub)
+                twilio_aux.send_custom_message(
+                    phone_number, 
+                    f"You are now subscribed to {streamer.twitch_name}!"
+                )
+
+    @commands.command()
+    async def add_streamer(self, ctx, twitch_name):
+        # checking that streamer exists
+        streamer: Twitch_Aux = Twitch_Aux(twitch_name)
+
+        if streamer.stream_info is None:
+            await ctx.send(
+                f"shieee, there isn't a twitch streamer named {twitch_name}\n"
+            )
+        else:
+            session = Session()
+            sm = StreamerModel(
+                twitch_name = streamer.twitch_name,
+                is_live = False
+            )
+
+            session.add(sm)
+            session.commit()
+            session.close()
+
+            await ctx.send(
+                f"{twitch_name} has been added to the system!"
+            )
 
 def setup(client):
     client.add_cog(Twitch_Notifications(client))
