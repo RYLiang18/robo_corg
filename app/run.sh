d76b73b7bdc439b0894c56fc98953809d8e0cd99
RYLiang18 <richard.y.liang@gmail.com>
2022-06-02 17:14:03 -0700

    dev: created Heroku Postgres, connected successfully thru heroku_postgres_test.py

---
+++ app/run.sh
@@ -0,0 +1,7 @@
+read -p 'CMD: ' usr_cmd
+
+docker build -t giich_robocorg_image .
+docker run -it --name giich_robocorg_container giich_robocorg_image:latest $usr_cmd
+
+docker container rm giich_robocorg_container
+docker image rm giich_robocorg_image:latest
